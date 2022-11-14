80f625cd3989de4f5022b1dd4620eb840ac12a3e
aleattene <attene.tomorrowdevs@gmail.com>
2022-11-14 21:15:30 +0100

    feat(built-in-objects): add array-every-method (part one)

---
+++ built-in-objects/array/methods/every.mjs
@@ -0,0 +1,12 @@
+/*
+The every() method tests whether all elements in the array pass the test implemented by the provided function.
+It returns a Boolean value.
+*/
+
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));  // expected: true
+
+console.log(`-`.repeat(40));
