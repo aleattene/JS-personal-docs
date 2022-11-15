bbf770d6549f808d385aa024ca8a78b1e18576a8
aleattene <attene.tomorrowdevs@gmail.com>
2022-11-15 21:45:44 +0100

    feat(built-in-objects): add array-every-method (part two)

--- built-in-objects/array/methods/every.mjs
+++ built-in-objects/array/methods/every.mjs
@@ -10,3 +10,38 @@ const array1 = [1, 30, 39, 29, 10, 13];
 console.log(array1.every(isBelowThreshold));  // expected: true
 
 console.log(`-`.repeat(40));
+
+/*
+ Syntax
+ - Arrow function
+    every((element) => { ... })
+    every((element, index) => { ... })
+    every((element, index, array) => { ... })
+
+ - Callback function
+    every(callbackFn)
+    every(callbackFn, thisArg)
+
+ - Inline callback function
+    every(function (element) { ... })
+    every(function (element, index) { ... })
+    every(function (element, index, array) { ... })
+    every(function (element, index, array) { ... }, thisArg)
+*/
+
+// TO FIX -> Parameters
+// TO FIX -> Return value
+// TO FIX -> Description
+
+
+// Examples
+// Testing size of all array elements
+// The following example tests whether all elements in the array are bigger than 10.
+function isBigEnough(element, index, array) {
+    return element >= 10;
+}
+
+console.log([12, 5, 8, 130, 44].every(isBigEnough));   // expected: false
+console.log([12, 54, 18, 130, 44].every(isBigEnough)); // expected: true
+
+console.log(`-`.repeat(40));
